b3750c995dbbec968e5c3ba38e8046508556964f
Kazikrystof <krystof.kazimour@gmail.com>
2026-07-21 16:45:05 +0200

    kick oprava TESTOVANI

--- scraper.py
+++ scraper.py
@@ -52,14 +52,14 @@ def start_scraper(callback):
 
         while True:
             try:
-                """ try:
+                try:
                     messages = page.locator("#chat-container p").all_inner_texts()
                     messages.reverse()
 
                     for msg in messages:
 
                             
-                        print(msg)
+                        
 
                         links = re.findall(
                             r"(?:https?://)?(?:www\.)?kick\.com/[^\s]+",
@@ -79,7 +79,7 @@ def start_scraper(callback):
                         save_kick_link(link)
 
                 except Exception:
-                    pass """
+                    pass 
 
                 # Aktualizace potu
                 try:
